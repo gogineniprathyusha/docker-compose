42026a997acc11c67cbb63d93597d8e1f6a097dd
gogineniprathyusha <104327062+gogineniprathyusha@users.noreply.github.com>
2022-11-05 14:16:39 -0400

    test

--- elkinstall.sh
+++ elkinstall.sh
@@ -7,6 +7,6 @@ chkconfig docker on
 yum install -y git
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-git clone hhttps://github.com/gogineniprathyusha/docker-compose.git
-cd /docker-compose
+git clone https://github.com/gogineniprathyusha/docker-compose.git
+cd /docker-compose/
 docker-compose -f docker-compose.yaml up -d
